5e787c2818b32866fcc66af18a5aafab50757642
Toromo A <toromo.ade06@gmail.com>
2022-11-01 09:38:34 -0500

    testing library

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-// import Sectionone from '../components/sectionone'
 import Hero from '../components/hero'
 
 const Home: NextPage = () => {
